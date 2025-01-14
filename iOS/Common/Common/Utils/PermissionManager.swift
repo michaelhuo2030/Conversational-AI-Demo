6cc75c0347aa7b2a03ba0c72122fe5645849ab9a
HeZhengQing <hezhengqing@agora.io>
2025-01-14 19:27:28 +0800

    [FIX] add common utils

---
+++ iOS/Common/Common/Utils/PermissionManager.swift
@@ -0,0 +1,37 @@
+//
+//  PermissionHelper.swift
+//  Agent
+//
+//  Created by HeZhengQing on 2024/9/29.
+//
+
+import UIKit
+import AVFAudio
+
+///This class is used to request system permissions.
+public class PermissionManager: NSObject {
+    public typealias MicrophonePermissionHandler = (Bool) -> Void
+
+    public static func checkMicrophonePermission(completion: @escaping MicrophonePermissionHandler) {
+        let audioSession = AVAudioSession.sharedInstance()
+        switch audioSession.recordPermission {
+        case .granted:
+            // The user has authorized
+            completion(true)
+        case .undetermined:
+            // The user has not made a choice to apply for permission.
+            audioSession.requestRecordPermission { granted in
+                DispatchQueue.main.async {
+                    completion(granted)
+                }
+            }
+        case .denied:
+            // The user rejected the permission.
+            completion(false)
+        @unknown default:
+            fatalError("Unknown permission status")
+        }
+    }
+
+    
+}
