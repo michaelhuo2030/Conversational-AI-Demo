54502b56e48bed734373b6cdde492e0d4aea9741
zhangwei <zw0872@163.com>
2025-02-11 21:41:23 +0800

    [Feat][Android]update anim video

--- Android/scenes/convoai/src/main/java/io/agora/scene/convoai/ui/CovSettingsDialog.kt
+++ Android/scenes/convoai/src/main/java/io/agora/scene/convoai/ui/CovSettingsDialog.kt
@@ -77,18 +77,7 @@ class CovSettingsDialog : BaseSheetDialog<CovSettingDialogBinding>() {
 
     private fun updatePageEnable() {
         val context = context ?: return
-        if (CovAgentManager.connectionState == AgentConnectionState.CONNECTED) {
-            binding?.apply {
-                tvPresetDetail.setTextColor(context.getColor(io.agora.scene.common.R.color.ai_icontext4))
-                tvLanguageDetail.setTextColor(context.getColor(io.agora.scene.common.R.color.ai_icontext4))
-                ivPresetArrow.setColorFilter(context.getColor(io.agora.scene.common.R.color.ai_icontext4), PorterDuff.Mode.SRC_IN)
-                ivLanguageArrow.setColorFilter(context.getColor(io.agora.scene.common.R.color.ai_icontext4), PorterDuff.Mode.SRC_IN)
-                clPreset.isEnabled = false
-                clLanguage.isEnabled = false
-                cbBhvs.isEnabled = false
-                cbAiVad.isEnabled = false
-            }
-        } else {
+        if (CovAgentManager.connectionState == AgentConnectionState.IDLE) {
             binding?.apply {
                 tvPresetDetail.setTextColor(context.getColor(io.agora.scene.common.R.color.ai_icontext1))
                 tvLanguageDetail.setTextColor(context.getColor(io.agora.scene.common.R.color.ai_icontext1))
@@ -98,6 +87,17 @@ class CovSettingsDialog : BaseSheetDialog<CovSettingDialogBinding>() {
                 clLanguage.isEnabled = true
                 cbBhvs.isEnabled = true
                 cbAiVad.isEnabled = true
+            }
+        } else {
+            binding?.apply {
+                tvPresetDetail.setTextColor(context.getColor(io.agora.scene.common.R.color.ai_icontext4))
+                tvLanguageDetail.setTextColor(context.getColor(io.agora.scene.common.R.color.ai_icontext4))
+                ivPresetArrow.setColorFilter(context.getColor(io.agora.scene.common.R.color.ai_icontext4), PorterDuff.Mode.SRC_IN)
+                ivLanguageArrow.setColorFilter(context.getColor(io.agora.scene.common.R.color.ai_icontext4), PorterDuff.Mode.SRC_IN)
+                clPreset.isEnabled = false
+                clLanguage.isEnabled = false
+                cbBhvs.isEnabled = false
+                cbAiVad.isEnabled = false
             }
         }
     }
