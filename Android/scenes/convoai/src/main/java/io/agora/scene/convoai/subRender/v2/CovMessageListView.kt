fa37517617767189fa8c56f350684cf812c2eda4
zhangwei <zw0872@163.com>
2025-02-24 22:01:17 +0800

    [Android] add interrupt status

--- Android/scenes/convoai/src/main/java/io/agora/scene/convoai/subRender/v2/CovMessageListView.kt
+++ Android/scenes/convoai/src/main/java/io/agora/scene/convoai/subRender/v2/CovMessageListView.kt
@@ -1,12 +1,18 @@
 package io.agora.scene.convoai.subRender.v2
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.text.Html
 import android.view.ViewGroup
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.agora.scene.common.util.dp
@@ -193,8 +199,39 @@ class CovMessageListView @JvmOverloads constructor(
         inner class AgentMessageViewHolder(private val binding: CovMessageAgentItemBinding) :
             MessageViewHolder(binding.root) {
             override fun bind(message: Message) {
-                binding.tvMessageContent.text = message.content
                 binding.tvMessageTitle.text = fromTitle
+
+                when (message.status) {
+                    SubtitleStatus.End -> {
+                        binding.tvMessageContent.text = message.content
+                    }
+
+                    SubtitleStatus.Interrupted -> {
+                        val htmlText =
+                            "${message.content} <img src=\"${io.agora.scene.common.R.drawable.ai_interrupt}\"/>"
+                        binding.tvMessageContent.text = HtmlCompat.fromHtml(
+                            htmlText,
+                            HtmlCompat.FROM_HTML_MODE_COMPACT,
+                            object : Html.ImageGetter {
+                                override fun getDrawable(source: String?): Drawable {
+                                    val drawable = ContextCompat.getDrawable(
+                                        binding.root.context,
+                                        io.agora.scene.common.R.drawable.ai_interrupt
+                                    ) ?: return ColorDrawable(Color.TRANSPARENT)
+
+                                    val size = binding.tvMessageContent.textSize.toInt()
+                                    drawable.setBounds(0, 0, size, size)
+                                    return drawable
+                                }
+                            },
+                            null
+                        )
+                    }
+
+                    else -> {
+                        binding.tvMessageContent.text = message.content
+                    }
+                }
             }
         }
 
