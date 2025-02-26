48cdd3756abb1fe3ab6de071b832e1bd6bc2f01c
qinhui <>
2025-02-26 22:29:13 +0800

    bugfix

--- iOS/Agent/src/SceneDelegate.swift
+++ iOS/Agent/src/SceneDelegate.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import VoiceAgent
+import SVProgressHUD
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        SVProgressHUD.setOffsetFromCenter(UIOffset(horizontal: 0, vertical: 180))
 
         let window = UIWindow(windowScene: windowScene)
 //        window.rootViewController = setupTabbarController()
